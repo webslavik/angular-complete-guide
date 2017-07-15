167602e9a5a32bf9aff9fea3bcbfdb4683068d3f
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-07-15 19:23:39 +0300

    added Ingredients module, markup and styles for Shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredients } from '../_shared/ingredients.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Pear', 3),
+    new Ingredients('Bananas', 7),
+    new Ingredients('Patatos', 4)
+  ];
 
   constructor() { }
 
