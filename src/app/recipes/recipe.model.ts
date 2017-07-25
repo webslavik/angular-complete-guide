e1548a8d68a6127b07e2e31b449114133c483520
Slavik <slavik.sukhanov@iskyweb.com>
2017-07-25 09:37:51 +0300

    update recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from '../_shared/ingredients.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, descr: string, imagePath: string) {
+  constructor(name: string, descr: string, imagePath: string, ingredient: Ingredient[]) {
     this.name = name;
     this.description = descr;
     this.imagePath = imagePath;
+    this.ingredients = ingredient;
   }
 }
